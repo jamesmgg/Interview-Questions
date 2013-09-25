224800e9986383bcaebb5f211e96d24dd5e17ffb
Jaime Guajardo <jaimemguajardo@gmail.com>
2013-09-25 17:19:44 -0500

    added new test

--- leastRecentlyUsedCache/Tests.java
+++ leastRecentlyUsedCache/Tests.java
@@ -44,5 +44,28 @@ public class Tests {
 		assertTrue(cache.get("sup") != null);
 		assertTrue(cache.get("yo") != null);
 	}
+	
+	// Special case where even though "hola" is also expired, only one item is removed everytime
+	// another is added.
+	@Test
+	public void test3() throws InterruptedException {
+		DeadElementCallBack<String, Integer> callback = new DeadElementCallBack<String, Integer>() {
+
+			@Override
+			public void notify(String key, Integer elements) {
+			}
+		}; 
+		LRUCache<String, Integer> cache = new LRUCache<String, Integer>(2000, 2, callback);
+		cache.put("hello", 1);
+		cache.put("yo", 2);
+		cache.put("hola", 4);
+		Thread.sleep(1500);
+		cache.get("yo"); // Using a value.
+		Thread.sleep(1000);
+		cache.put("sup", 3);
+		assertTrue(cache.size() == 3);
+		assertTrue(cache.get("sup") != null);
+		assertTrue(cache.get("yo") != null);
+	}
 
 }
